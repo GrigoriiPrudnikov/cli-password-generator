dd54ac41901e485cbfaa69cdcb2645daabb8a79f
Grigorii Prudnikov <griprudnikov742@gmail.com>
2023-08-10 23:00:07 +0500

    add comments

--- src/getRandomInt.js
+++ src/getRandomInt.js
@@ -1,3 +1,4 @@
+// Function to generate a random integer between 0 (inclusive) and max (exclusive)
 export default function getRandomInt(max) {
 	return Math.floor(Math.random() * max)
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,42 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk'
-import inquirer from 'inquirer'
-import getRandomInt from './getRandomInt.js'
+// Import necessary modules
+import chalk from 'chalk' // For colorful console output
+import inquirer from 'inquirer' // For user input prompts
+import getRandomInt from './getRandomInt.js' // Function to generate random integers
 
-let symbols = 'abcdefjhijklmnopqrstuvwxyz'
+let symbols = 'abcdefjhijklmnopqrstuvwxyz' // Default symbols for password generation
 
+// Ask the user for the length of the password
 const passwordLength = await inquirer.prompt({
 	name: 'answer',
 	type: 'number',
 	message: `Enter length of password: `,
 })
+
+// Check if the entered value is a number
 if (Number.isNaN(passwordLength.answer)) {
 	console.log(chalk.bgRed('It is not a number!'))
-	process.exit(1)
+	process.exit(1) // Exit the script with an error code
 }
+
+// Ask the user if they want to use capital letters in the password
 const isCapitals = await inquirer.prompt({
 	name: 'answer',
 	type: 'list',
 	message: 'Do you want to use capital letters in your password?',
 	choices: ['yes', 'no'],
 })
+
+// Ask the user if they want to use numbers in the password
 const isNumbers = await inquirer.prompt({
 	name: 'answer',
 	type: 'list',
 	message: 'Do you want to use numbers in your password?',
 	choices: ['yes', 'no'],
 })
+
+// Ask the user if they want to use special symbols in the password
 const isSpecialSymbols = await inquirer.prompt({
 	name: 'answer',
 	type: 'list',
@@ -34,12 +44,17 @@ const isSpecialSymbols = await inquirer.prompt({
 	choices: ['yes', 'no'],
 })
 
+// Modify the symbols based on user choices
 if (isCapitals.answer === 'yes') symbols += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 if (isNumbers.answer === 'yes') symbols += '1234567890'
 if (isSpecialSymbols.answer === 'yes') symbols += '!@#$%&*?'
 
 let password = ''
+
+// Generate the password using the selected symbols
 for (let i = 0; i < passwordLength.answer; i++) {
-	password += symbols[getRandomInt(symbols.length)]
+	password += symbols[getRandomInt(symbols.length)] // Append a random symbol from the modified symbols string
 }
+
+// Display the generated password
 console.log(`Your password is ${chalk.magenta(password)}`)
